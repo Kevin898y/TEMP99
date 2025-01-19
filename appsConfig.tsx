24f8321ced8d4ce020cbb6cff960c6057ac6a42a
Kevin898y <kevin888y@gmail.com>
2025-01-20 04:42:55 +0800

    Add files via upload

--- appsConfig.tsx
+++ appsConfig.tsx
@@ -5,6 +5,7 @@ export interface AppConfig {
   name: string;
   icon: string; // 使用字符串存储图标名称
   link: string;
+  category: string; // 新增类别字段
 }
 
 export const appsConfig: AppConfig[] = [
@@ -13,35 +14,41 @@ export const appsConfig: AppConfig[] = [
     name: "App Store",
     icon: "FaAppStore", // 字符串表示图标
     link: "https://www.apple.com/app-store/",
+    category: "Tool",
   },
   {
     id: 2,
     name: "Google Play",
     icon: "FaGooglePlay",
     link: "https://play.google.com/",
+    category: "Shopping",
   },
   {
     id: 3,
     name: "Google Play",
     icon: "FaGooglePlay",
     link: "https://play.google.com/",
+    category: "Shopping",
   },
   {
     id: 4,
     name: "Google Play",
     icon: "FaGooglePlay",
     link: "https://play.google.com/",
+    category: "Shopping",
   },
   {
     id: 5,
     name: "Google Play",
     icon: "FaGooglePlay",
     link: "https://play.google.com/",
+    category: "Shopping",
   },
   {
     id: 6,
     name: "Google Play",
     icon: "FaGooglePlay",
     link: "https://play.google.com/",
+    category: "Shopping",
   },
 ];
